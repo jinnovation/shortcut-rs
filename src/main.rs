cfbc14164dd0366d938db11b2227ec06e6364019
Jonathan Jin <me@jonathanj.in>
2025-12-18 09:56:43 -0500

    implement basic config

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use config::{Config, ConfigError};
+
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+pub(crate) struct ShortcutConfig {
+    api_url: String,
+    api_key: String,
+}
+
+impl ShortcutConfig {
+    pub(crate) fn new() -> Result<Self, ConfigError> {
+        Config::builder()
+            .add_source(config::Environment::with_prefix("SHORTCUT"))
+            .set_default("api_url", "https://api.app.shortcut.com/api/v3")?
+            .build()?
+            .try_deserialize()
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let settings = ShortcutConfig::new().unwrap();
+
+    println!("api_url: {:?}", settings.api_url);
 }
