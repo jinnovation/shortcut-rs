7d2d84db917e8763999d81a533926a20d8a331c9
Jonathan Jin <me@jonathanj.in>
2025-12-18 11:25:45 -0500

    set up basic command tree

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use config::{Config, ConfigError};
 
+use clap::{Parser, Subcommand};
+
 use serde::Deserialize;
 
 #[derive(Deserialize)]
 #[allow(unused)]
 pub(crate) struct ShortcutConfig {
     api_url: String,
-    api_key: String,
+    // api_key: String,
 }
 
 impl ShortcutConfig {
@@ -19,8 +21,55 @@ impl ShortcutConfig {
     }
 }
 
+#[derive(Parser)]
+#[command(about = "Manage stories, epics, and projects in Shortcut.", long_about=None, arg_required_else_help=true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Work with Shortcut stories.")]
+    Stories {
+        #[command(subcommand)]
+        command: StoriesSubcommand,
+    },
+    #[command(about = "Work with Shortcut epics.")]
+    Epics {
+        #[command(subcommand)]
+        command: EpicsSubcommand,
+    },
+}
+
+#[derive(Subcommand)]
+enum StoriesSubcommand {
+    List,
+}
+
+#[derive(Subcommand)]
+enum EpicsSubcommand {
+    List,
+}
+
 fn main() {
     let settings = ShortcutConfig::new().unwrap();
+
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Some(Commands::Stories { command }) => match *command {
+            StoriesSubcommand::List => {
+                println!("stories list")
+            }
+        },
+        Some(Commands::Epics { command }) => match *command {
+            EpicsSubcommand::List => {
+                println!("epics list")
+            }
+        },
+        None => {}
+    }
 
     println!("api_url: {:?}", settings.api_url);
 }
