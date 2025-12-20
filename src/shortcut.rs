4cda3159831630fec1cc19a9ca113328ca84aa2c
Jonathan Jin <me@jonathanj.in>
2025-12-19 20:35:46 -0500

    add more story fields

--- src/shortcut.rs
+++ src/shortcut.rs
@@ -4,7 +4,9 @@ pub mod client;
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct Story {
-    pub id: i64,
+    pub id: i32,
     pub name: String,
     pub owner_ids: Vec<String>,
+    pub story_type: String,
+    pub epic_id: Option<i32>,
 }
